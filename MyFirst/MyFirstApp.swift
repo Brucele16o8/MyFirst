e13fedb70517407fa451d1b24dff2cc990a6e372
Bruce Le <brucele.tt168@gmail.com>
2023-11-25 00:23:30 +1100

    Initial Commit

---
+++ MyFirst/MyFirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstApp.swift
+//  MyFirst
+//
+//  Created by Tung Le on 25/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
